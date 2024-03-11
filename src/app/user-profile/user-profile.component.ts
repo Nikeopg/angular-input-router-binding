d983ef5afd42e584f79a37f5df2a4c102a19fbf3
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-03-11 13:40:47 +0100

    final init

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,47 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../user.service';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { map, switchMap } from 'rxjs';
+import { DataTableComponent } from '../data-table/data-table.component';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [JsonPipe, AsyncPipe],
+  imports: [AsyncPipe, DataTableComponent],
   template: `
     <main>
-      @if (profile$ | async; as user) {
-        <h1>{{user.name}}</h1>
-        <ul class="user-data">
-          <li>
-            <span class="key">ID:</span>
-            <span class="value">{{user.id}}</span>
-          </li>
-          <li>
-            <span class="key">Email:</span> 
-            <span class="value">{{user.email}}</span>
-          </li>
-          <li>
-            <span class="key">Address:</span> 
-            <span class="value">
-              {{user.address.zipcode}},
-              {{user.address.city}},
-              {{user.address.street}},
-              {{user.address.suite}}
-            </span>
-          </li>
-        </ul>
+      @if (profile$ | async; as profile) {
+        <h1>{{profile.name}}</h1>
+        <app-data-table [data]="profile" />
       }
     </main>
   `
 })
 export class UserProfileComponent {
   #activatedRoute = inject(ActivatedRoute);
+  #user = inject(UserService);
+
   userId$ = this.#activatedRoute.params.pipe(
     map(params => params['userId'])
   );
-
-  #user = inject(UserService);
 
   profile$ = this.userId$.pipe(
     switchMap(id => this.#user.profile(id))
