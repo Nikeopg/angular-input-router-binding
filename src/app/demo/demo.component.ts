787d2fb1b7d5efc627fda7b4aa7bd003492f5272
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-03-16 17:55:09 +0100

    added stuff to initial state

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-demo',
   standalone: true,
+  imports: [RouterLink],
   template: `
     <main>
       <h4>Router Param (entityId): <span>{{entityId}}</span></h4>
       <h4>Router Query Param (sort): <span>{{sort}}</span></h4>
       <h4>Router Data (title): <span>{{title}}</span></h4>
     </main>
+    <a class="demo-button" [routerLink]="['../', 4444]">Click to Change EntityId</a>
   `
 })
 export class DemoComponent implements OnChanges {
